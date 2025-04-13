5eb0f0c3add1b40ced305c4488a7eeded61eb50f
Bolivar Delgado <bolivar.delgado@gmail.com>
2025-04-13 13:32:45 -0500

    feat: add cookie-parser

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
-import express, { Request, Response, Express } from "express";
+import express, { Request, Response, Express, NextFunction, ErrorRequestHandler } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser"
 import { registerRoutes } from "./routes";
 import { errorHandler } from "./utils/error.handler";
 
@@ -11,11 +12,15 @@ const API_VERSION = "/api/v1";
 
 app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.disable("x-powered-by");
 
 registerRoutes(app, API_VERSION);
 
-app.use(errorHandler); 
+// Error handling middleware should be the last middleware
+app.use(((err: Error, req: Request, res: Response, next: NextFunction) => {
+  return errorHandler(err, req, res, next);
+}) as ErrorRequestHandler);
 
 export default app;
